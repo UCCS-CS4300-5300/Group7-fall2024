4fc6b45e9d4e9f6ef8f387aadf7e17a51cad0ba5
Daniel Buck <dbuck3@uccs.edu>
2024-10-25 00:49:40 +0000

    models are working, added the ability to add items through admin

--- home/views.py
+++ home/views.py
@@ -4,6 +4,13 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib import messages
 from .models import RAM, CPU, Motherboard, Storage
 from django.db.models import Q
+from rest_framework import viewsets
+from .models import Build
+from .serializers import BuildSerializer
+
+class BuildViewSet(viewsets.ModelViewSet):
+    queryset = Build.objects.all()
+    serializer_class = BuildSerializer
 
 def index(request):
     return render(request, 'index.html')
