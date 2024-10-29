a5f8977b044f130ab52e977f26141460e8c08c31
root <joelflinn@gmail.com>
2024-10-29 06:50:00 +0000

    tests for ram model

--- home/tests/test_models.py
+++ home/tests/test_models.py
@@ -97,4 +97,83 @@ class CPUTestCase(TestCase):
         expected_CPU_info=f"{testObject.cpu_name}"
         self.assertEqual(str(testObject), expected_CPU_info)
 
-    # testing the contents of the 
+    # testing the value of cpu_id
+    def test_CPU_object_CPU_id_values(self):
+        testObject = CPU.objects.get(cpu_id=1)
+        expected_CPU_info=f"{testObject.cpu_id}"
+        self.assertEqual("1", expected_CPU_info)
+
+    # testing the value of cpu_manufacturer
+    def test_CPU_object_CPU_manufacturer_values(self):
+        testObject = CPU.objects.get(cpu_id=1)
+        expected_CPU_info=f"{testObject.cpu_manufacturer.name}"
+        self.assertEqual("Intel", expected_CPU_info)
+
+    # testing the value of cpu_manufacturer
+    def test_CPU_object_CPU_microarchitecture_values(self):
+        testObject = CPU.objects.get(cpu_id=1)
+        expected_CPU_info=f"{testObject.cpu_microarchitecture.name}"
+        self.assertEqual("Zen 4", expected_CPU_info)
+
+    # testing the value of socket_type
+    def test_CPU_object_CPU_socket_type_values(self):
+        testObject = CPU.objects.get(cpu_id=1)
+        expected_CPU_info=f"{testObject.socket_type.name}"
+        self.assertEqual("AM5", expected_CPU_info)
+
+
+class RAMTestCase(TestCase):
+    # Set up non-modified objects used by all test methods
+    @classmethod
+    def setUpTestData(cls):
+        # create objects necesary to create a ram object
+        RAMType.objects.create(type="DDR4")
+        RAMSpeed.objects.create(speed="3200MHz")
+        RAMCapacity.objects.create(capacity="16GB")
+        RAMNumberOfModules.objects.create(number_of_modules=2)
+
+        # create ram object and establish relationships
+        RAM.objects.create(ram_id=1,
+        ram_type=RAMType.objects.get(id=1),
+        ram_speed=RAMSpeed.objects.get(id=1),
+        ram_capacity=RAMCapacity.objects.get(id=1),
+        ram_number_of_modules=RAMNumberOfModules.objects.get(id=1))
+
+        # testing the labels of the fields
+    def test_RAM_Ram_id_field_label(self):
+        testObject = RAM.objects.get(ram_id=1)
+        field_label = testObject._meta.get_field("ram_id").verbose_name
+        self.assertEqual(field_label, "ram id")
+
+    def test_RAM_type_field_label(self):
+        testObject = RAM.objects.get(ram_id=1)
+        field_label = testObject._meta.get_field("ram_type").verbose_name
+        self.assertEqual(field_label, "ram type")
+
+    def test_RAM_speed_field_label(self):
+        testObject = RAM.objects.get(ram_id=1)
+        field_label = testObject._meta.get_field("ram_speed").verbose_name
+        self.assertEqual(field_label, "ram speed")
+
+    def test_RAM_capacity_field_label(self):
+        testObject = RAM.objects.get(ram_id=1)
+        field_label = testObject._meta.get_field("ram_capacity").verbose_name
+        self.assertEqual(field_label, "ram capacity")
+
+    def test_RAM_number_of_modules_label(self):
+        testObject = RAM.objects.get(ram_id=1)
+        field_label = testObject._meta.get_field("ram_number_of_modules").verbose_name
+        self.assertEqual(field_label, "ram number of modules")
+
+    # testing the value of ram_id
+    def test_RAM_object_RAM_id_values(self):
+        testObject = RAM.objects.get(ram_id=1)
+        expected_RAM_info=f"{testObject.ram_id}"
+        self.assertEqual("1", expected_RAM_info)
+
+    # testing the values of the ram_type, ram_speed, ram_capacity, and ram_number_of_modules
+    def test_RAM_object_fields_values(self):
+        testObject = RAM.objects.get(ram_id=1)
+        expected_storage_info=f"{testObject.ram_type} {testObject.ram_speed} - {testObject.ram_number_of_modules} x {testObject.ram_capacity}"
+        self.assertEqual(str(testObject), expected_storage_info)
+
