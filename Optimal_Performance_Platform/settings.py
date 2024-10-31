206a6935aca821c1ae782b13acbf26291f2503b4
Tiehack <nhallise@uccs.edu>
2024-10-31 20:04:45 +0000

    Added myself to the settings file

--- Optimal_Performance_Platform/settings.py
+++ Optimal_Performance_Platform/settings.py
@@ -171,7 +171,8 @@ DEBUG = True
 
 # Will need to add everyone here or figure out a solution
 
-ALLOWED_HOSTS = ["app-jzlotoff-5.devedu.io", "app-jflinn2-5.devedu.io", "app-jmeredet-5.devedu.io", "app-dbuck3-5.devedu.io",]
+ALLOWED_HOSTS = ["app-jzlotoff-5.devedu.io", "app-jflinn2-5.devedu.io", "app-jmeredet-5.devedu.io", "app-dbuck3-5.devedu.io",
+"app-nhallise-5.devedu.io"]
 
 
 # Application definition
@@ -253,6 +254,7 @@ CSRF_TRUSTED_ORIGINS = [
     'https://app-jflinn2-5.devedu.io',
     'https://app-dbuck3-5.devedu.io',
     'https://app-jmeredet-5.devedu.io',
+    'https://app-nhallise-5.devedu.io'
 ]
 
 
