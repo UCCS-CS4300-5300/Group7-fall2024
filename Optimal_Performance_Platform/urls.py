af07d630bcd6f5705a888e284ab500a8f7ab210c
Tiehack <nhallise@uccs.edu>
2024-11-09 22:10:11 +0000

    Upda

--- Optimal_Performance_Platform/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from home import views  # Import the entire views module from home
-
-urlpatterns = [
-    path('admin/', admin.site.urls),               # Admin site
-    path('api/', include('home.urls_api')),        # Include home app API URLs prefixed with /api/
-    path('', include('home.urls')),                # Include home app non-API URLs
-]
